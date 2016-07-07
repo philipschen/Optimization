84faef759b571af6c241e855f21ac65610079938
Philip Chen <philipschen@hotmail.com>
2016-07-07 14:51:35 -0400

    efficiency update and bugfixes

--- Optimization/UsedStockInputExcel.vb
+++ Optimization/UsedStockInputExcel.vb
@@ -16,7 +16,7 @@ Public Class UsedStockInputExcel
 
         fd.Title = "Open File Dialog"
         fd.InitialDirectory = "C:\"
-        fd.Filter = "Excell files (*.xls, *.xlsx)|*.xls;*.xlsx"
+        fd.Filter = "Excel files (*.xls, *.xlsx)|*.xls;*.xlsx"
         fd.FilterIndex = 2
         fd.RestoreDirectory = True
         fd.Multiselect = False
@@ -147,9 +147,11 @@ Public Class UsedStockInputExcel
             Label1.Text = "Loading Complete"
                 DataGridView1.AutoResizeColumns()
                 readerObj.Close()
-                wbXl.Close()
-                appXL.Quit()
-            End If
+            shXL = Nothing
+            wbXl = Nothing
+            appXL.Quit()
+            appXL = Nothing
+        End If
 
     End Sub
 
@@ -160,13 +162,75 @@ Public Class UsedStockInputExcel
         con.ConnectionString = "Data Source=TOSHIBA-2015\SQLEXPRESS;Initial Catalog=OptimizationDatabase;Integrated Security=True"
         con.Open()
         cmd.Connection = con
-
+        Dim con1 As New SqlConnection
+        Dim cmd1 As New SqlCommand
+        con1.ConnectionString = "Data Source=TOSHIBA-2015\SQLEXPRESS;Initial Catalog=OptimizationDatabase;Integrated Security=True"
+        con1.Open()
+        cmd1.Connection = con1
+        Dim ar0 As ArrayList = New ArrayList
+        Dim ar1 As ArrayList = New ArrayList
+        Dim ar2 As ArrayList = New ArrayList
+        Dim ar3 As ArrayList = New ArrayList
+        Dim ar4 As ArrayList = New ArrayList
+        Dim ar5 As ArrayList = New ArrayList
+        Dim ar6 As ArrayList = New ArrayList
+        Dim ar7 As ArrayList = New ArrayList
+        Dim ar8 As ArrayList = New ArrayList
+        Dim ar9 As ArrayList = New ArrayList
+        Dim ar10 As ArrayList = New ArrayList
+        Dim ar11 As ArrayList = New ArrayList
         For it = 0 To DataGridView1.RowCount - 1
-            If Not String.Equals(DataGridView1.Rows(it).Cells(1).Value, "") And DataGridView1.Rows(it).Cells(1).Value IsNot Nothing Then
-                cmd.CommandText = "INSERT INTO stockUsed VALUES('" + DataGridView1.Rows(it).Cells(0).Value.ToString.ToUpper() + "', '" + DataGridView1.Rows(it).Cells(1).Value.ToString.ToUpper() + "' , '" + DataGridView1.Rows(it).Cells(2).Value.ToString.ToUpper() + "', '" + DataGridView1.Rows(it).Cells(3).Value.ToString.ToUpper() + "' , '" + DataGridView1.Rows(it).Cells(4).Value.ToString.ToUpper() + "', " + DataGridView1.Rows(it).Cells(5).Value.ToString.ToUpper() + " , " + DataGridView1.Rows(it).Cells(6).Value.ToString.ToUpper() + ", " + DataGridView1.Rows(it).Cells(7).Value.ToString.ToUpper() + ", '" + DataGridView1.Rows(it).Cells(8).Value.ToString.ToUpper() + "', '" + DataGridView1.Rows(it).Cells(9).Value.ToString.ToUpper() + "'," + DataGridView1.Rows(it).Cells(10).Value.ToString.ToUpper() + ",'" + DataGridView1.Rows(it).Cells(11).Value.ToString.ToUpper() + "')"
-                cmd.ExecuteNonQuery()
+            If Not String.Equals(DataGridView1.Rows(it).Cells(3).Value, "") And DataGridView1.Rows(it).Cells(3).Value IsNot Nothing Then
+                ar0.Add(DataGridView1.Rows(it).Cells(0).Value.ToString.ToUpper())
+                ar1.Add(DataGridView1.Rows(it).Cells(1).Value.ToString.ToUpper())
+                ar2.Add(DataGridView1.Rows(it).Cells(2).Value.ToString.ToUpper())
+                ar3.Add(DataGridView1.Rows(it).Cells(3).Value.ToString.ToUpper())
+                ar4.Add(DataGridView1.Rows(it).Cells(4).Value.ToString.ToUpper())
+                ar5.Add(DataGridView1.Rows(it).Cells(5).Value.ToString.ToUpper())
+                ar6.Add(DataGridView1.Rows(it).Cells(6).Value.ToString.ToUpper())
+                ar7.Add(DataGridView1.Rows(it).Cells(7).Value.ToString.ToUpper())
+                ar8.Add(DataGridView1.Rows(it).Cells(8).Value.ToString.ToUpper())
+                ar9.Add(DataGridView1.Rows(it).Cells(9).Value.ToString.ToUpper())
+                ar10.Add(DataGridView1.Rows(it).Cells(10).Value.ToString.ToUpper())
+                ar11.Add(DataGridView1.Rows(it).Cells(11).Value.ToString.ToUpper())
             End If
         Next
+        cmd.CommandText = "SELECT stockID2, size, count, context2  FROM stockUsed"
+        cmd.ExecuteNonQuery()
+        Dim readerObj As SqlClient.SqlDataReader = cmd.ExecuteReader
+        While readerObj.Read
+            For it = 0 To ar1.Count - 1
+                If String.Equals(ar1(it), readerObj("stockID2").ToString) And String.Equals(ar5(it), readerObj("size").ToString) Then
+                    'Dim temp1 As Integer = Convert.ToInt32(ar6(it))
+                    'Dim temp2 As String = readerObj("count").ToString
+                    'Dim temp3 As Integer = Convert.ToInt32(temp2)
+                    'temp3 = temp3 + temp1
+                    'temp2 = Convert.ToString(temp3)
+                    cmd1.CommandText = "UPDATE stockUsed SET count = " + ar6(it) + " WHERE context2 = " + readerObj("context2").ToString
+                    cmd1.ExecuteNonQuery()
+                    ar0.RemoveAt(it)
+                    ar1.RemoveAt(it)
+                    ar2.RemoveAt(it)
+                    ar3.RemoveAt(it)
+                    ar4.RemoveAt(it)
+                    ar5.RemoveAt(it)
+                    ar6.RemoveAt(it)
+                    ar7.RemoveAt(it)
+                    ar8.RemoveAt(it)
+                    ar9.RemoveAt(it)
+                    ar10.RemoveAt(it)
+                    ar11.RemoveAt(it)
+                    Exit For
+                End If
+            Next
+        End While
+
+        For it = 0 To ar1.Count - 1
+            cmd1.CommandText = "INSERT INTO stockUsed VALUES('" + ar0(it) + "', '" + ar1(it) + "' , '" + ar2(it) + "', '" + ar3(it) + "' , '" + ar4(it) + "', " + ar5(it) + " , " + ar6(it) + ", " + ar7(it) + ", '" + ar8(it) + "', '" + ar9(it) + "'," + ar10(it) + ",'" + ar11(it) + "')"
+            cmd1.ExecuteNonQuery()
+        Next
+
+
         Label1.Text = "Upload Complete"
         Dim frmNewExtr As uploadComplete = New uploadComplete()
         frmNewExtr.ShowDialog()
