fc1f6c9e9ee09172d2ac5422e95d2994d5cd3ab9
Philip Chen <philipschen@hotmail.com>
2016-08-03 10:49:15 -0400

    test

--- Optimization/Class1.vb
+++ Optimization/Class1.vb
@@ -1,4 +1,4 @@
 ﻿Public Class Class1
     Public Property connect1 As String = "Data Source=TOSHIBA-2015\SQLEXPRESS;Initial Catalog=OptimizationDatabase;Integrated Security=True"
-    Public Property version As String = "Easy Cut V1.1"
+    Public Property version As String = "Easy Cut V1.1 "
 End Class
